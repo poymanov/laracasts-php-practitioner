e3a55e3b771135a0cc2f9c64cfce1516d4f0efc4
Nikolay Poymanov <n.poymanov@gmail.com>
2017-12-11 23:09:58 +0300

    Fix connection class

--- database/Connection.php
+++ database/Connection.php
@@ -5,7 +5,7 @@ class Connection
 	public static function make($config)
 	{
 		try {
-			return new PDO("mysql:host={$config['localhost']};dbname={$config['database']}", $config['user'], $config['password'], $config['options']);
+			return new PDO("mysql:host={$config['host']};dbname={$config['database']}", $config['user'], $config['password'], $config['options']);
 		} catch(PDOException $e) {
 			die($e->getMessage());
 		}		
